4f705146cee69d1afd361a92e505454943229784
Yevgeniy Ten <foodila@hotmail.com>
2021-01-09 22:17:22 +0500

    #1 Lecture writer 0.1 version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [texts, setTexts] = useState({
+        text1: "",
+        text2: ""
+    })
+    const onChangeText = e => {
+        const {name, value} = e.target
+        setTexts(prev => ({...prev, [name]: value}))
+    }
+    return (
+        <div className="container">
+            <div className="row">
+                <textarea value={texts.text1}
+                          onChange={onChangeText}
+                          className="text"
+                          rows="10"
+                          name="text1"
+                          placeholder="Левая часть"/>
+                <textarea value={texts.text2}
+                          name="text2"
+                          onChange={onChangeText}
+                          className="text"
+                          rows="10"
+                          placeholder="Правая часть"/>
+            </div>
+            <div className="row">
+                <div className="notebook">
+                    <pre className="block-1">
+                        {texts.text1}
+                    </pre>
+                    <pre className="block-2">
+                       {texts.text2}
+                    </pre>
+                </div>
+            </div>
+        </div>
+    )
 }
-
 export default App;
